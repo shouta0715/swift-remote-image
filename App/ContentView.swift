53ccfe0e08795ba57a24e97140bc1dbcef2b47d0
shouta0715 <Myheroes0715@gmail.com>
2024-12-28 23:41:31 +0900

    initili

---
+++ App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  remote-image
+//
+//  Created by k21047kk on 2024/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ App/remote_imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  remote_imageApp.swift
+//  remote-image
+//
+//  Created by k21047kk on 2024/12/28.
+//
+
+import SwiftUI
+
+@main
+struct remote_imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
