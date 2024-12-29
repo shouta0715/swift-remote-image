5f7b7746ff4139286a85e498c7d821f3616c2f8b
shouta0715 <Myheroes0715@gmail.com>
2024-12-29 11:06:45 +0900

    fix: extension image modifier

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+extension Image {
+    func imageModifier() -> some View {
+        self.resizable().scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
     
     private let imageURL:String = "https://credo.academy/credo-academy@3x.png"
@@ -14,21 +28,12 @@ struct ContentView: View {
     
     var body: some View {
         
-//        1. Basic
-//        AsyncImage(url:URL(string: imageURL))
-        
-//        2. scale Image
-//        AsyncImage(url:URL(string: imageURL),scale: 3.0)
-        
-//        3. 3. PlaceHolder
         AsyncImage(url: URL(string:imageURL)){image in
-            image.resizable().scaledToFit()
+            image.imageModifier()
         } placeholder: {
             Image(systemName: "photo.circle.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: 128)
-                .foregroundColor(.purple).opacity(0.5)
+                .iconModifier()
+
         }.padding(40)
         
     }
